0bf04797518fcd101a4e7ef84a6a450ef3e0b0b2
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-03-15 00:18:48 -0500

    feat: basic math type generation

---
+++ src/GoldMeridian.Mathematics.SourceGen.Private/Generators/MathGenerator.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace GoldMeridian.Mathematics.SourceGen.Generators;
+
+[Generator]
+public sealed class MathGenerator : IIncrementalGenerator
+{
+    void IIncrementalGenerator.Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(
+            ctx =>
+            {
+                foreach (var vector in TypeSpecs.Vectors)
+                {
+                    ctx.AddSource(
+                        $"{vector.TypeName}.vector.g.cs",
+                        VectorEmitter.Generate(vector)
+                    );
+                }
+
+                /*
+                foreach (var matrix in TypeSpecs.Matrices)
+                {
+                    ctx.AddSource(
+                        $"{matrix.TypeName}.matrix.g.cs",
+                        MatrixEmitter.Generate(matrix)
+                    );
+                }
+                */
+            }
+        );
+    }
+}
---
+++ src/GoldMeridian.Mathematics.SourceGen.Private/Generators/VectorEmitter.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace GoldMeridian.Mathematics.SourceGen.Generators;
+
+internal static class VectorEmitter
+{
+    public static readonly string[] LANE_NAMES = ["X", "Y", "Z", "W"];
+
+    public static string Generate(VectorSpec spec)
+    {
+        var lanes = LANE_NAMES.Take(spec.Lanes).ToArray();
+        var fields = string.Join("\n", lanes.Select(x => $"public {spec.Scalar.TypeOrKeywordName} {x} = {x};"));
+        var ctorParams = string.Join(", ", lanes.Select(x => $"{spec.Scalar.TypeOrKeywordName} {x}"));
+
+        return $$"""
+                 namespace GoldMeridian.Mathematics;
+                 
+                 public partial struct {{spec.TypeName}}({{ctorParams}})
+                 {
+                 {{fields}}
+                 }
+                 """;
+    }
+}
